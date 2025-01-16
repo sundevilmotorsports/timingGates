d4c9cedfce4eb0de72724b641dc13a12532984aa
Kaden Greenlief <kgreenl3@asu.edu>
2025-01-15 22:04:18 -0700

    Implemented radio code (doesn't send packets yet)

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,50 @@
 #include <Arduino.h>
 #include <TimeLib.h>
+#include <RH_RF95.h>
 
-time_t RTCTime;
+#define CS0 10
+#define G00 23
+#define SCK 13
+
+#define MODEM_CONFIG RH_RF95::ModemConfigChoice::Bw125Cr45Sf128
 
 #define HWSERIAL Serial1
 #define CENTIMETERS 1000
 #define TIMEOUT 3000
 
+time_t RTCTime;
+RH_RF95 driver(CS0, G00);
+
 int starting_micro = 0;
 int starting_time = 0;
+bool radio = false;
 
 time_t getTeensy3Time() {
   return Teensy3Clock.get();
 }
 
 void setup() {
+  pinMode(SCK, OUTPUT);
+
   Serial.begin(9600);
   HWSERIAL.begin(115200);
+  
   setSyncProvider(getTeensy3Time);
   Serial.println("\n" __FILE__ " " __DATE__ " " __TIME__);
   int start_sec = second();
   while(start_sec == second());
   starting_micro = micros();
   starting_time = now();
+  
+  radio = driver.init();
+  if (!radio)
+    Serial.println("init 1 failed"); 
+  else {
+    Serial.println("init 1 succeded");
+    driver.setFrequency(915.0); // Median of Hz range
+    driver.setTxPower(RH_RF95_MAX_POWER, false); //Max power, should increase range, but try to find min because a little rude to be blasting to everyone
+    driver.setModemConfig(MODEM_CONFIG); //Bandwidth of 125, Cognitive Radio 4/5, Spreading Factor 2048
+  }
 }
 
 
@@ -57,14 +79,22 @@ int strength = 0;
 int start = 0;
 bool waiting = false;
 
+uint8_t general[8];
+
 void loop() {
   getTFminiData(&distance, &strength);
   if(!waiting && distance < CENTIMETERS){
     start = millis();
     waiting = true;
-  } else if (TIMEOUT && millis() - start > TIMEOUT){
+    Serial.println(String(distance) + "cm\tstrength: " + String(strength) + "\tstart: " + String(start));
+    bool sent = false;
+    if(radio){
+      sent = driver.send(general, sizeof(general));
+    }
+    Serial.println("Sent radio 1: " + String(sent) + "\t micros: " + String(micros()) + "\tstarting delta: " + String(micros()-starting_micro));
+  } else if (waiting && millis() - start > TIMEOUT){
     waiting = false;
+    Serial.println("ended timeout");
+    // Serial.println("Current: " + String(month()) +"/" + String(day()) + "/" + String(year()) + "\t" + String(hour()) + ":" + String(minute()) + ":" + String(second()) + "\tSeconds Since 1970: " + String(now()) + "\tMicros: " + String(micros()) + "\tStarting Seconds: " + String(starting_time) + "\tStarting micro: " + String(starting_micro));
   }
-  Serial.println("Current: " + String(month()) +"/" + String(day()) + "/" + String(year()) + "\t" + String(hour()) + ":" + String(minute()) + ":" + String(second()) + "\tSeconds Since 1970: " + String(now()) + "\tMicros: " + String(micros()) + "\tStarting Seconds: " + String(starting_time) + "\tStarting micro: " + String(starting_micro));
-  Serial.println(String(distance) + "cm\tstrength: " + String(strength) + "\tstart: " + String(start));
 }
